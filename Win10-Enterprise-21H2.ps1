d772eff53871c6f0302d513bf2e10aa851a58631
jasonjonasbergen <93658825+jasonjonasbergen@users.noreply.github.com>
2022-05-27 10:14:32 -0500

    Update Win10-Enterprise-21H2.ps1

--- Win10-Enterprise-21H2.ps1
+++ Win10-Enterprise-21H2.ps1
@@ -1,18 +1,18 @@
-Write-Host -ForegroundColor Green "Starting OSDCloud ZTI RETSD 806"
+Write-Host -ForegroundColor Green "Starting OSDCloud Zero Touch Installation"
 Start-Sleep -Seconds 5
 
-#Make sure I have the latest OSD Content
-#Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
-#Install-Module OSD -Force
-
-#Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
-#Import-Module OSD -Force
-
-#Start OSDCloud ZTI the RIGHT way
+#Start OSDCloud ZTI
 Write-Host -ForegroundColor Green "Start OSDCloud"
-Start-OSDCloud -OSBuild "21H2" -OSLanguage en-us -OSEdition Enterprise -ZTI -OSVersion "Windows 10" -Product "0B15"
+Start-OSDCloud -OSBuild "21H2" -OSLanguage en-us -OSEdition Enterprise -ZTI -OSVersion "Windows 10"
+
+Write-Host -ForegroundColor Red "                          ########################################################"
+Write-Host -ForegroundColor Blue "                          ########################################################"
+Write-Host -ForegroundColor Green "                          *****  REMOVE USB KEY... PRESS ANY KEY TO CONTINUE *****"
+Write-Host -ForegroundColor Blue "                          ########################################################"
+Write-Host -ForegroundColor Red "                          ########################################################"
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
 #Restart from WinPE
-#Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
-#Start-Sleep -Seconds 20
-#wpeutil reboot
+Write-Host -ForegroundColor Green "Restarting in 10 seconds!"
+Start-Sleep -Seconds 10
+wpeutil reboot
