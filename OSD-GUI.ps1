b6bd6f5b20854575b61fdf02f5b9e70aceaa7e5b
Jason Bergen <jbergen@retsd.mb.ca>
2022-08-18 14:51:45 -0500

    changed a bunch of stuff

--- OSD-GUI.ps1
+++ OSD-GUI.ps1
@@ -1,76 +1,33 @@
-Import-Module -Name OSD -Force
-#=================================================
-#   Get-FeatureUpdates
-#=================================================
-$Results = Get-WSUSXML -Catalog FeatureUpdate -Silent
-$Results = $Results | Where-Object {$_.UpdateArch -eq 'x64'}
-$Results = $Results | Select-Object `
-@{Name='Catalog';Expression={('OS')}}, `
-@{Name='Updated';Expression={(Get-Date -Format "yyyy-MM-dd")}}, `
-@{Name='Status';Expression={($_.OSDStatus)}}, `
-@{Name='Released';Expression={(Get-Date $_.CreationDate -Format "yyyy-MM-dd")}}, `
-@{Name='Name';Expression={($_.Title)}}, `
-@{Name='OS';Expression={($null)}}, `
-@{Name='Version';Expression={($_.UpdateBuild)}}, `
-@{Name='Build';Expression={($null)}}, `
-@{Name='Arch';Expression={($_.UpdateArch)}}, `
-@{Name='Language';Expression={($null)}}, `
-@{Name='Activation';Expression={($null)}}, `
-@{Name='FileName';Expression={((Split-Path -Leaf $_.FileUri))}}, `
-@{Name='ImageIndex';Expression={($null)}}, `
-@{Name='ImageName';Expression={($null)}}, `
-@{Name='Url';Expression={($_.FileUri)}}, `
-@{Name='SHA1';Expression={($null)}}, `
-@{Name='UpdateID';Expression={($_.UpdateID)}}, `
-@{Name='Win10';Expression={($null)}}, `
-@{Name='Win11';Expression={($null)}}
+#Replaces faulty Function
+Remove-Item -path Function:\Test-MicrosoftUpdateCatalog
+function Test-MicrosoftUpdateCatalog {
+    [CmdletBinding()]
+    param ()
 
-foreach ($Result in $Results) {
-    #=================================================
-    #   Language
-    #=================================================
-    if ($Result.FileName -match 'sr-latn-rs') {
-        $Result.Language = 'sr-latn-rs'
+    $StatusCode = (Invoke-WebRequest -uri 'https://www.catalog.update.microsoft.com' -UseBasicParsing -Method Head -ErrorAction Ignore).StatusCode 
+
+    if ($StatusCode -eq 200) {
+        Return $true
+    } else {
+        Return $false
     }
-    else {
-        $Regex = "[a-zA-Z]+-[a-zA-Z]+"
-        $Result.Language = ($Result.FileName | Select-String -AllMatches -Pattern $Regex).Matches[0].Value
-    }
-    #=================================================
-    #   Activation
-    #=================================================
-        $Result.Activation = 'Retail'
-    #=================================================
-    #   OS
-    #=================================================
-    if ($Result.Name -match 'Windows 10') {
-        $Result.OS = 'Windows 10'
-        $Result.Win10 = $true
-        $Result.Win11 = $false
-    }
-    if ($Result.Name -match 'Windows 11') {
-        $Result.OS = 'Windows 11'
-        $Result.Win10 = $false
-        $Result.Win11 = $true
-    }
-    #=================================================
-    #   Build
-    #=================================================
-    $Regex = "[0-9]*\.[0-9]+"
-    $Result.Build = ($Result.FileName | Select-String -AllMatches -Pattern $Regex).Matches[0].Value
-    #=================================================
-    #   SHA1
-    #=================================================
-    $Regex = "[0-9a-f]{40}"
-    $Result.SHA1 = ($Result.FileName | Select-String -AllMatches -Pattern $Regex).Matches[0].Value
-    #=================================================
-    #   Name
-    #=================================================
-    $Result.Name = $Result.OS + ' ' + $Result.Version + ' x64 ' + $Result.Language + ' consumer editions ' + $Result.Build
-    #=================================================
 }
-$Results = $Results | Sort-Object -Property Name
-#$Results | Write-Output x:\output.TEXT
-$Results | Export-Clixml -Path (Join-Path (Get-Module OSD).ModuleBase "Catalogs\operatingsystems.xml") -Force
-Import-Clixml -Path (Join-Path (Get-Module OSD).ModuleBase "Catalogs\operatingsystems.xml") | ConvertTo-Json | Out-File (Join-Path (Get-Module OSD).ModuleBase "Catalogs\operatingsystems.json") -Force -Encoding ascii
-#================================================
+
+Write-Host -ForegroundColor Green "Testing GUI changes"
+Start-Sleep -Seconds 5
+
+#Start OSDCloud ZTI
+Write-Host -ForegroundColor Green "Start OSDCloud"
+Start-OSDCloud -OSBuild "21H2" -OSLanguage en-us -OSEdition Pro -OSLicense Retail -OSVersion "Windows 10" -Firmware
+Write-Host " "
+Write-Host -ForegroundColor Red "                          ########################################################"
+Write-Host -ForegroundColor Blue "                          ########################################################"
+Write-Host -ForegroundColor Green "                          *****  REMOVE USB KEY... PRESS ANY KEY TO CONTINUE *****"
+Write-Host -ForegroundColor Blue "                          ########################################################"
+Write-Host -ForegroundColor Red "                          ########################################################"
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+#Restart from WinPE
+Write-Host -ForegroundColor Green "Restarting in 10 seconds!"
+Start-Sleep -Seconds 10
+wpeutil reboot
